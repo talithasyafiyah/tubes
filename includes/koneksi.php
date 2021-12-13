d9f8dbdb17d27e332c0404948d7c74e0d89a1c3d
Febri Zuladhari <febrizuladhari12345@gmail.com>
2021-12-13 21:34:41 +0700

    bismillah push

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -9,7 +9,7 @@
 
     if(!$koneksi){
     die ("Koneksi dengan database gagal: ".mysqli_connect_errno().
-         " - ".mysqli_connect_error());
-     }
+        " - ".mysqli_connect_error());
+    }
 
 ?>
